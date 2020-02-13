8407e9528740a2ff131b3f43eb593d187d1ff36e
Psi-Hate <Psi-Hate@users.noreply.github.com>
2020-02-13 02:02:45 -0600

    Update Interfaces.ts

--- cores/MajorasMask/API/Interfaces.ts
+++ cores/MajorasMask/API/Interfaces.ts
@@ -80,6 +80,8 @@ export interface IPlayer {
   rot_x: number;
   rot_y: number;
   rot_z: number;
+  
+  link_object: number;
 }
 
 export interface IRuntime {
@@ -92,6 +94,7 @@ export interface IRuntime {
   cutscene_ptr: number;
   scene_flags: IBuffered;
   scene_frame: number;
+
 }
 
 export interface ISaveContext {
